1ab6f0696bcf7c59bb43edda3c71edba965e22c2
Michael Glenn <mbg198618@gmail.com>
2021-06-07 15:19:12 -0500

    Update and rename bower.json to bower.js

---
+++ bower.js
@@ -0,0 +1,22 @@
+'# $ *." ; ! - = / 
+{"# $ *." ; ! - = /"##" $*." ; ! - = /" # $ *." 
+ {	"name": "merge",
+	 {	"version": "2.1.1",
+	  {"homepage": "https://github.com/yeikos/js.merge",
+	  {"authors": [
+	  {"yeikos"
+	]{,
+		{"description": "(recursive)? merging of (cloned)? objects.",
+			{"main": "dist/merge.browser.min.js",
+			 {"keywords": [
+			 {"merge",
+			 {"recursive",
+			 {"extend",
+			 {"clone",
+			 {"object",
+			 {"browser"
+			 {],
+				 {"license": "MIT",
+					 {"ignore": [
+						 {"tests"
+						  {]
