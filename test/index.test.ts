5319e759ce972c7f37fdbaff4aa3521d01f876a9
Juanra GM <juanrgm724@gmail.com>
2023-05-05 08:26:03 +0200

    test: use `toStrictEqual`

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,18 @@
 import merge from "../src/index";
-import { describe, it, assert, expect } from "vitest";
+import { describe, it, expect } from "vitest";
 
-describe("merge", function () {
-  it("basic", () => {
-    assert.deepEqual(merge({ a: 1 }, { b: 2 }), { a: 1, b: 2 });
+describe("merge", () => {
+  it("merges two objects", () => {
+    expect(merge({ a: 1 }, { b: 2 })).toStrictEqual({ a: 1, b: 2 });
+  });
 
-    assert.deepEqual(merge({ a: 1 }, { b: { c: { d: 2 } } }), {
+  it("merges nested levels", () => {
+    expect(merge({ a: 1 }, { b: { c: { d: 2 } } })).toStrictEqual({
       a: 1,
       b: { c: { d: 2 } },
     });
   });
-
-  it("clone", () => {
+  it("clones the target", () => {
     let input = {
       a: 1,
       b: {
@@ -44,59 +45,57 @@ describe("merge", function () {
     (input.b.c.e[2] as any).z.w = null;
     (input as any).h = null;
 
-    assert.deepEqual(original, output);
+    expect(output).toStrictEqual(original);
 
     input = original;
 
     output = merge(true, input, { a: 2 });
 
-    assert.deepEqual(output.a, 2);
-    assert.deepEqual(input.a, 1);
+    expect(output.a).toBe(2);
+    expect(input.a).toBe(1);
   });
 
-  it("invalid input", () => {
-    assert.deepEqual(merge(), {});
-
-    assert.deepEqual(merge(undefined), {});
-
-    assert.deepEqual(merge([]), {});
-
-    assert.deepEqual(merge(true), {});
-
-    assert.deepEqual(
+  it("ignores the sources", () => {
+    expect(merge()).toStrictEqual({});
+    expect(merge(undefined)).toStrictEqual({});
+    expect(merge([])).toStrictEqual({});
+    expect(merge(true)).toStrictEqual({});
+    expect(
       merge(null, true, [0, 1, 2], 3, { a: 1 }, function () {}, undefined, {
         b: 2,
-      }),
-      { a: 1, b: 2 }
-    );
+      })
+    ).toStrictEqual({ a: 1, b: 2 });
   });
 
-  it("prototype pollution attack", () => {
-    assert.deepEqual(merge({}, JSON.parse('{"__proto__": {"a": true}}')), {});
+  it("is safe", () => {
+    expect(
+      merge({}, JSON.parse('{"__proto__": {"evil": true}}'))
+    ).toStrictEqual({});
+    expect(({} as any).evil).toBeUndefined();
   });
 });
 
-describe("merge.clone", function () {
-  it("object", () => {
+describe("merge.clone", () => {
+  it("clones the input", () => {
     const object1 = { a: 1, b: { c: 2 } };
     const object2 = merge.clone(object1);
 
-    assert.deepEqual(object1, object2);
+    expect(object1).toStrictEqual(object2);
     expect(object1 === object2).toBeFalsy();
     expect(object1.b === object2.b).toBeFalsy();
   });
 
-  it("array", () => {
+  it("clones each item of the array", () => {
     const object1 = [{ a: 1, b: { c: 2 } }];
     const object2 = merge.clone(object1);
 
-    assert.deepEqual(object1, object2);
+    expect(object1).toStrictEqual(object2);
     expect(object1 === object2).toBeFalsy();
     expect(object1[0] === object2[0]).toBeFalsy();
     expect(object1[0].b === object2[0].b).toBeFalsy();
   });
 
-  it("invalid input", () => {
+  it("returns the same input", () => {
     expect(merge.clone(null)).toBeNull();
     expect(merge.clone(undefined)).toBeUndefined();
     expect(merge.clone(1)).toBe(1);
@@ -104,77 +103,70 @@ describe("merge.clone", function () {
   });
 });
 
-describe("merge.recursive", function () {
-  it("basic", () => {
-    assert.deepEqual(merge.recursive({ a: { b: 1 } }, { a: { c: 1 } }), {
+describe("merge.recursive", () => {
+  it("merges recursively", () => {
+    expect(merge.recursive({ a: { b: 1 } }, { a: { c: 1 } })).toStrictEqual({
       a: { b: 1, c: 1 },
     });
 
-    assert.deepEqual(merge.recursive({ a: { b: 1, c: 1 } }, { a: { b: 2 } }), {
+    expect(
+      merge.recursive({ a: { b: 1, c: 1 } }, { a: { b: 2 } })
+    ).toStrictEqual({
       a: { b: 2, c: 1 },
     });
 
-    assert.deepEqual(
-      merge.recursive({ a: { b: [1, 2, 3], c: 1 } }, { a: { b: ["a"] } }),
-      { a: { b: ["a"], c: 1 } }
-    );
+    expect(
+      merge.recursive({ a: { b: [1, 2, 3], c: 1 } }, { a: { b: ["a"] } })
+    ).toStrictEqual({ a: { b: ["a"], c: 1 } });
 
-    assert.deepEqual(
-      merge.recursive({ a: { b: { b: 2 }, c: 1 } }, { a: { b: 2 } }),
-      { a: { b: 2, c: 1 } }
-    );
+    expect(
+      merge.recursive({ a: { b: { b: 2 }, c: 1 } }, { a: { b: 2 } })
+    ).toStrictEqual({
+      a: { b: 2, c: 1 },
+    });
   });
 
-  it("clone", function () {
+  it("clones recursively", function () {
     const test1 = { a: { b: 1 } };
 
-    assert.deepEqual(merge.recursive(true, test1, { a: { c: 1 } }), {
+    expect(merge.recursive(true, test1, { a: { c: 1 } })).toStrictEqual({
       a: { b: 1, c: 1 },
     });
 
-    assert.deepEqual({ a: { b: 1 } }, test1);
+    expect(test1).toStrictEqual({ a: { b: 1 } });
 
     const test2 = { a: { b: 1, c: 1 } };
 
-    assert.deepEqual(merge.recursive(true, test2, { a: { b: 2 } }), {
+    expect(merge.recursive(true, test2, { a: { b: 2 } })).toStrictEqual({
       a: { b: 2, c: 1 },
     });
 
-    assert.deepEqual({ a: { b: 1, c: 1 } }, test2);
+    expect(test2).toStrictEqual({ a: { b: 1, c: 1 } });
 
     const test3 = { a: { b: [1, 2, 3], c: 1 } };
 
-    assert.deepEqual(merge.recursive(true, test3, { a: { b: ["a"] } }), {
+    expect(merge.recursive(true, test3, { a: { b: ["a"] } })).toStrictEqual({
       a: { b: ["a"], c: 1 },
     });
 
-    assert.deepEqual({ a: { b: [1, 2, 3], c: 1 } }, test3);
+    expect(test3).toStrictEqual({ a: { b: [1, 2, 3], c: 1 } });
 
     const test4 = { a: { b: { b: 2 }, c: 1 } };
 
-    assert.deepEqual(merge.recursive(true, test4, { a: { b: 2 } }), {
+    expect(merge.recursive(true, test4, { a: { b: 2 } })).toStrictEqual({
       a: { b: 2, c: 1 },
     });
 
-    assert.deepEqual({ a: { b: { b: 2 }, c: 1 } }, test4);
+    expect(test4).toStrictEqual({ a: { b: { b: 2 }, c: 1 } });
   });
 
-  it("prototype pollution attack", function () {
-    assert.deepEqual(
-      merge.recursive({}, JSON.parse('{"__proto__": {"a": true}}')),
-      {}
-    );
-
+  it("is safe", function () {
+    const payload = '{"__proto__": {"a": true}}';
+    expect(merge.recursive({}, JSON.parse(payload))).toStrictEqual({});
     expect(({} as any)["a"]).toBeUndefined();
-
-    assert.deepEqual(
-      merge.recursive(
-        { deep: {} },
-        JSON.parse('{ "deep": { "__proto__": {"b": true} }}')
-      ),
-      { deep: {} }
-    );
-
+    expect(merge.recursive({ deep: {} }, JSON.parse(payload))).toStrictEqual({
+      deep: {},
+    });
     expect(({} as any)["b"]).toBeUndefined();
   });
 });
